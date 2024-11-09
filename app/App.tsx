4111af09f808ba66544d6285194861274ad6179a
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-11-09 20:32:35 +0500

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,40 @@
-import { View, Text ,StyleSheet,TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react';
 
 const App = () => {
+  const rowone = ['e', 'μ', 'sin', 'deg'];
+  const rowtwo=['Ac','x','/','*'];
+  const rowthree=['']
   return (
-    <View>
-      <Text>App</Text>
+    <View style={Styles.container}>
+   
     </View>
-  )
-}
+  );
+};
 
-export default App
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  RowOne: {
+    flexDirection: 'row',
+  
+    justifyContent: 'space-between',
+  },
+  item: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    marginHorizontal: 5,
+    borderRadius: 5,
+  },
+  itemText: {
+    fontSize: 18,
+    color: '#333',
+  },
+});
+
+export default App;
