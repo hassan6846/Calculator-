eb01a48d5eb72f051ab9d6758bbcce1f01802bf2
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-11-09 13:55:56 +0500

    main

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import { View, Text ,StyleSheet,TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
